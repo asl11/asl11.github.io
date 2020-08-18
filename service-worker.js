6f3c6c0ec01f5a61c0dca98cd1926a01d6cf5105
asl11 <asl11@rice.edu>
2020-08-17 20:03:27 -0700

    fixed bug causing opacity to go to 1%

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2c9eb02b18a175d67de3d42def5e2bd2.js"
+  "/precache-manifest.d7c42cc78c5a0bf36bcdc1ea97d84e7a.js"
 );
 
 self.addEventListener('message', (event) => {
